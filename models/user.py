0839bc6e9f76b097d3cc51e31ec51ddae06c5a25
Mohakad <mohkadtrd@gmail.com>
2024-03-19 18:31:20 +0300

    1test

--- models/user.py
+++ models/user.py
@@ -2,11 +2,12 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
-from os import getenv
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 class User(BaseModel, Base):
     '''
