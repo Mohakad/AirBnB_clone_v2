acb00407ea86ecdf8ddca4bd7d661ad60eb69012
Mohakad <mohkadtrd@gmail.com>
2024-04-20 20:38:44 +0300

    1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""starts a Flask web application port 5000"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """print hello bnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbn():
+    """print hbnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    """if main"""
+    app.run(host='0.0.0.0', port=5000)
